893a5752d9f5b1c23e75fb71e0ac0d4ce6c8f206
Chandu J S <app.coder@outlook.in>
2019-03-18 22:32:34 +0530

    ui also added

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class MovieList extends StatelessWidget {
+import 'package:flutter_bloc/src/blocs/movies_bloc.dart';
+import 'package:flutter_bloc/src/models/item_model.dart';
+
+class MovieList extends StatefulWidget {
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    moviesBloc.fetchAllMovies();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: null,
+      appBar: AppBar(
+        title: Text('Popular Movies'),
+      ),
+      body: StreamBuilder(
+        stream: moviesBloc.allMovies,
+        builder: (_, AsyncSnapshot<ItemModel> snapshot) {
+          if (snapshot.hasData) {
+            return buildList(snapshot);
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
+    return GridView.builder(
+      itemCount: snapshot.data.results.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (BuildContext context, int index) {
+        return Image.network(
+          'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].posterPath}',
+          fit: BoxFit.cover,
+        );
+      }
     );
   }
 }
